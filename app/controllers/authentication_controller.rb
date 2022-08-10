c9504cb4ce1365612ef1805373378e7f5e0efad6
Paa Yaw <paayaw.dev@gmail.com>
2022-08-10 18:36:58 +0000

    authorize all api requests except signup and login endpoints

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,4 +1,6 @@
 class AuthenticationController < ApplicationController
+  skip_before_action :authorize_request, only: :authenticate
+    
   def authenticate
     auth_token = AuthenticateUser.new(user_params[:email], user_params[:password]).call
     json_response({ auth_token: auth_token })
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,53 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
   let(:user) { build(:user) }
   let(:valid_attributes) do
-    { 
+    {
       username: user.username,
       email: user.email,
-      password: user.password, 
+      password: user.password,
       password_confirmation: user.password
     }.to_json
   end
 
-  let(:invalid_attributes) do 
+  let(:invalid_attributes) do
     {
-      
+
     }.to_json
   end
 
   let(:headers) { valid_headers.except('Authorizaton') }
-  
-  describe 'Users#create' do 
-    context 'successful signup' do 
+
+  describe 'Users#create' do
+    context 'successful signup' do
       before { post '/signup', params: valid_attributes, headers: headers }
 
-      it 'should return auth_token' do 
+      it 'should return auth_token' do
         expect(json['auth_token']).not_to be_nil
       end
 
-      it 'should return positive message' do  
+      it 'should return positive message' do
         expect(json['message']).to eq('Account created successfully')
       end
 
-      it 'return 201 status code' do 
+      it 'return 201 status code' do
         expect(response).to have_http_status(201)
       end
     end
 
-    context 'unsuccessful signup' do 
+    context 'unsuccessful signup' do
       before { post '/signup', params: invalid_attributes, headers: headers }
 
-      it 'should return no auth_token' do 
+      it 'should return no auth_token' do
         expect(json['auth_token']).to be_nil
       end
 
-      it 'should return positive message' do  
+      it 'should return positive message' do
         expect(json['message']).to eq("Validation failed: Password can't be blank, Username can't be blank, Email can't be blank, Password digest can't be blank")
       end
 
-      it 'return 201 status code' do 
+      it 'return 201 status code' do
         expect(response).to have_http_status(422)
       end
     end
